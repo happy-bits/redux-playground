fbeba21b11bd4a2437510960a84b29ec4c2142a0
Oscar Olsson <happybits@gmail.com>
2024-12-05 15:40:01 +0100

    12

--- src/pages/Page04.js
+++ src/pages/Page04.js
@@ -1,5 +1,29 @@
+import { useDispatch } from "react-redux"
+import { adjustAmountBy } from "../features/mylist/mylistSlice"
+
 export default function Page04() {
+
+    const dispatch = useDispatch()
+
     return (
-        <h1>Page 04</h1>
+        <>
+            <h1>Page 04</h1>
+
+            <button
+
+                onClick={() => {
+                    dispatch(
+
+                        adjustAmountBy({
+                            id: 1,
+                            diff: 1000
+                        })
+
+                    )
+                }}
+            >
+                Add 1000 pieces to product with id=1
+            </button>
+        </>
     )
 }
